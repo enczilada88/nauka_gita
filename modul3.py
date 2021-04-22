2a0aa835a876513d3a96e5549362d907842f2f65
enczilada88 <pacek.sylwia@gmail.com>
2021-04-22 13:58:58 +0200

    Init project2

--- modul3.py
+++ modul3.py
@@ -4,4 +4,12 @@ shopping_list={
     "warzywiak": ["marchewka", "cebula","sałata"],
     "mięsny": ["schab", "szynka"]
 }
-print(shopping_list)
+number_of_products=[]
+for shop, products in shopping_list.items():
+    for n in products:
+        Products=[]
+        for n in products:
+            Products.append(n.capitalize())
+    print(f"Idę do {shop.capitalize()} i kupuję tam: {Products}")
+    number_of_products.append(len(products))
+print("W sumie kupuję " + str(sum(number_of_products))+" produktów")
