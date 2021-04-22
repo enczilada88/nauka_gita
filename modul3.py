55137efd6a47561b91a005f0564831a2761e4ee9
enczilada88 <pacek.sylwia@gmail.com>
2021-04-22 13:32:01 +0200

    Init project

---
+++ modul3.py
@@ -0,0 +1,7 @@
+#zadanie1
+shopping_list={
+    "piekarnia": ["bagietka", "kajzerka", "grahamka","chałka"],
+    "warzywiak": ["marchewka", "cebula","sałata"],
+    "mięsny": ["schab", "szynka"]
+}
+print(shopping_list)
